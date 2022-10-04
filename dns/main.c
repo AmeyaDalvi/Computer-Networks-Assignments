6f259cb6100fa32e48fefbbc6c1d98adef4b23b2
Dalvi, Ameya Bhushan <abdalvi@iu.edu>
2022-10-04 18:30:54 -0400

    forgot to push my code on git

--- dns/main.c
+++ dns/main.c
@@ -1,10 +1,17 @@
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 /*
   Use the `getaddrinfo` and `inet_ntop` functions to convert a string host and
   integer port into a string dotted ip address and port.
  */
+
 int main(int argc, char* argv[]) {
   if (argc != 3) {
     printf("Invalid arguments - %s <host> <port>", argv[0]);
@@ -17,6 +24,50 @@ int main(int argc, char* argv[]) {
     STUDENT CODE HERE
    */
 
+  // https://www.educative.io/answers/how-to-use-the-sprintf-method-in-c
+  // Converting port (service) to char*
+
+  char service[100];
+  sprintf(service, "%ld", port);
+
+  // Creating the "hints" 
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = PF_UNSPEC;    /* Allow IPv4 or IPv6 */
+  hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+  hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+  hints.ai_protocol = IPPROTO_TCP;
+
+
+  struct addrinfo *response;
+
+  int res = getaddrinfo(host, service, &hints, &response);
+
+  if (res != 0) {
+    printf("Some error");
+  }
+
+  for(struct addrinfo *i = response; i!=NULL; i= i->ai_next){
+    void* raw_addr;
+
+    if (i->ai_family == AF_INET) { // Address is IPv4
+      int sszie_t = INET_ADDRSTRLEN;
+      char dst[sszie_t];
+      struct sockaddr_in* tmp = (struct sockaddr_in*)i->ai_addr; // Cast addr into AF_INET container
+      raw_addr = &(tmp->sin_addr); // Extract the address from the container
+      inet_ntop(i->ai_family, raw_addr, dst, sszie_t);
+      printf("IPv4 %s\n",dst);
+    }
+    else { // Address is IPv6
+      int sszie_t = INET6_ADDRSTRLEN;
+      char dst[sszie_t];
+      struct sockaddr_in6* tmp = (struct sockaddr_in6*)i->ai_addr; // Cast addr into AF_INET6 container
+      raw_addr = &(tmp->sin6_addr); // Extract the address from the container
+      inet_ntop(i->ai_family, raw_addr, dst, sszie_t);
+      printf("IPv6 %s\n",dst);
+    }
+    /* Use raw_addr as a generic address for inet_ntop */
+  }
 
   return 0;
 }
--- smtp/main.c
+++ smtp/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int connect_smtp(const char* host, int port);
 void send_smtp(int sock, const char* msg, char* resp, size_t len);
@@ -18,15 +19,71 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 
-  // char* rcpt = argv[1];
-  // char* filepath = argv[2];
+  char* rcpt = argv[1];
+  char* filepath = argv[2];
 
   /* 
      STUDENT CODE HERE
    */
 
   int socket = connect_smtp("lunar.open.sice.indiana.edu", 25);
+  FILE* file_ptr;
+  char ch;
+  file_ptr = fopen(filepath, "r");
+  char email_body[4096];
 
-  printf("%d",socket);
+
+  if (NULL == file_ptr) {
+    printf("file can't be opened \n");
+  }
+
+
+// Refered from: https://www.geeksforgeeks.org/c-program-to-read-contents-of-whole-file/
+// https://stackoverflow.com/questions/22621952/convert-char-to-string-in-c
+
+  while (!feof(file_ptr)) {
+      char cstr[2];
+      cstr[1] = '\0';
+      ch = fgetc(file_ptr);
+      cstr[0] = ch;
+      strcat(email_body, cstr);
+  }
+
+  int size = strlen(email_body);
+  email_body[size-1] = '\0';
+
+  printf("%d",size);
+
+  strcat(email_body, "\r\n.\r\n"); //To append the period at the end of file
+  printf("%s",email_body);
+  fclose(file_ptr);
+
+  // printf("%d",socket);
+  
+  char mail[100] = "MAIL FROM: ";
+  char rcpt_to[100] = "RCPT TO: ";
+  char response[4096];
+  //HELO
+  send_smtp(socket, "HELO iu.edu\n", response, 4096);
+  printf("%s\n", response);
+
+  //MAIL FROM
+  strcat(mail, rcpt);
+  strcat(mail, "\n");
+  send_smtp(socket, mail, response, 4096);
+  printf("%s\n", response);
+
+  //RCPT TO
+  strcat(rcpt_to, rcpt);
+  strcat(rcpt_to, "\n");
+  send_smtp(socket, rcpt_to, response, 4096);
+  printf("%s\n", response);
+
+  //DATA
+  send_smtp(socket, "DATA\n", response, 4096);
+  send_smtp(socket, email_body, response, 4096);
+  printf("%s\n", response);
+  
   return 0;
+
 }
